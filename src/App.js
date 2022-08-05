dbc9093713c84aa25c32130f28e6472281c2a5f4
mohamedfazilk <2114921@cognizant.com>
2022-08-05 10:26:53 +0530

    inline data is mapping

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+const response_data =[
+
+  {
+    id:1,
+    title:"Sample article1",
+    content:"Long simple content",
+    created_at:'12/12/12 01:20:02',
+    updated_at:'12/12/12 01:20:02',
+  },
+
+  {
+    id:2,
+    title:"Sample article2",
+    content:"Long simple content2",
+    created_at:'12/12/12 01:20:02',
+    updated_at:'12/12/12 01:20:02',
+  }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <article>
+    {response_data.map(({id,title,content,created_at,updated_at}) =>{
+      return(
+        <article key={id}>
+
+          <h1>{title}</h1>
+          <h3>{content}</h3>
+          <small>{created_at}</small>
+          <small>{updated_at}</small>
+
+        </article>
+      )
+    } )}
+  </article>
 }
 
 export default App;
